6340dfd1eb3c8ec3fb48106a60048d222c4950ca
sreejith <squeeky.mouse@gmail.com>
2023-03-02 00:27:57 +0530

    got polarity and graph Sec35

--- main_my.py
+++ main_my.py
@@ -2,33 +2,37 @@ import streamlit as st
 import plotly.express as px
 import glob
 from pathlib import Path
-# from datetime import datetime
+from datetime import datetime
 from nltk.sentiment import SentimentIntensityAnalyzer
-# commit: got polarity and graph Sec35
+# commit: Initial commit Sec35
 
 st.title('Diary Tone')
 
-filepaths=sorted(glob.glob('diary/*.txt'))
+filepaths=glob.glob('diary/*.txt')
+filepaths=sorted(filepaths) #get filepaths sorted probably by date
 analyzer=SentimentIntensityAnalyzer()
 
-list_date=[file.strip('.txt').strip('diary/') for file in filepaths] # no need to format date as plotly will parse date itself!!!
-# list_date=[datetime.strptime(Path(filepath).stem, '%Y-%m-%d').strftime('%b %d') for filepath in filepaths]
-
+list_date=[]
 list_pos_tone=[]
 list_neg_tone=[]
 for filepath in filepaths:
+    ddate=datetime.strptime(Path(filepath).stem, '%Y-%m-%d').strftime('%b %d')
+    list_date.append(ddate)
     with open(filepath) as file:
         diary=file.read()
-    score=analyzer.polarity_scores(diary)
-    list_pos_tone.append(score['pos'])
-    list_neg_tone.append(score['neg'])
+        # print(analyzer.polarity_scores(diary))
+        score=analyzer.polarity_scores(diary)
+        list_pos_tone.append(score['pos'])
+        list_neg_tone.append(score['neg'])
 
 st.subheader('Positivity')
+# chart
 fig_pos=px.line(x=list_date,y=list_pos_tone,
                 labels={'x':'Date','y':'Positivity'})
 st.plotly_chart(fig_pos)
 
 st.subheader('Negativity')
+# chart
 fig_neg=px.line(x=list_date,y=list_neg_tone,
                 labels={'x':'Date','y':'Negativity'})
 st.plotly_chart(fig_neg)
